e0b135ec9435f7438b1994d272339cf3c01ac66b
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-17 17:16:06 -0500

    Created FormatSong function to return the formatted string

--- WriteSongFormat.cs
+++ WriteSongFormat.cs
@@ -1,6 +1,11 @@
-﻿namespace playlistimport;
+﻿using Utilities;
+namespace playlistimport;
 
 public class WriteSongFormat
 {
-    
+    public static string FormatSong(Song song)
+    {
+        var message = String.Format("{0}  |  {1}  |  {2}  | Plays:{3}",song.Name,song.Artist, song.Genre, song.Plays);
+        return message;
+    }
 }
